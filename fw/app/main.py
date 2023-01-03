669aa878f92bcb73d8e73e211f7c587969719056
Pavel Burgr <pb@drake.cz>
2023-01-03 18:19:05 +0100

    WIP

--- fw/app/main.py
+++ fw/app/main.py
@@ -1,6 +1,7 @@
 import esp_wifi
+import wifi_config
 
 print('EASUN SMP Telemetry')
 
-esp_wifi.init(ssid = 'xxx', password='xxx', ledPin = 4, ledLogic = True)
+esp_wifi.init(ssid = wifi_config.ssid, password=wifi_config.password, ledPin = 4, ledLogic = True)
 
