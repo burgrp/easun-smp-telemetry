5877df0e9399427804ac494a5a2477b2a871cfe3
Pavel Burgr <pb@drake.cz>
2023-01-04 22:26:39 +0100

    WIP

--- fw/app/main.py
+++ fw/app/main.py
@@ -1,18 +1,31 @@
-import sys
-sys.path.append('dep')
-
 import mqtt_call
 import site_config
+from machine import UART
 
 print('EASUN SMP Telemetry')
 print('Inverter index:', site_config.inverter_index)
 
-class Handler:
-    def add(self, a, b):
-        return a+b
+END_OF_LINE = '\n'
 
-    async def div(self, a, b):
-        return a/b
+class Handler:
+
+    uart = None
+
+    def __init__(self):
+        self.uart = UART(1, baudrate=2400, tx=21, rx=20, timeout=5000, timeout_char=5000)
+
+
+    def export_message(self, text):
+        while self.uart.any() > 1:
+            self.uart.read(1)
+
+        self.uart.write(text + END_OF_LINE)
+
+        line = self.uart.readline();
+        if line != None:
+            line = line.decode()
+
+        return (line or "").strip()
 
 
 server = mqtt_call.Server(
@@ -21,7 +34,7 @@ server = mqtt_call.Server(
     wifi_ssid=site_config.wifi_ssid,
     wifi_password=site_config.wifi_password,
     mqtt_broker=site_config.mqtt_broker,
-    debug=False
+    debug=True
 )
 
 server.dump()
